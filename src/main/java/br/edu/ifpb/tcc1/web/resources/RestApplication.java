cc67f0e97b7cb5727b2f59b8cfd28e7515a1fce4
FlavioHenriique <flaviohenrique638@gmail.com>
2018-09-13 19:14:23 -0300

    Adicionando projeto

---
+++ src/main/java/br/edu/ifpb/tcc1/web/resources/RestApplication.java
@@ -0,0 +1,9 @@
+package br.edu.ifpb.tcc1.web.resources;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class RestApplication extends Application{
+    
+}
