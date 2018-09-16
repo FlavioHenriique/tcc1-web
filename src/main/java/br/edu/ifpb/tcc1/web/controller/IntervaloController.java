acd8e89eae346c98186155712ce8ce4b699e7783
FlavioHenriique <flaviohenrique638@gmail.com>
2018-09-15 21:38:24 -0300

    Implementando busca por meses no front end, comunicando-se com a api

--- src/main/java/br/edu/ifpb/tcc1/web/controller/IntervaloController.java
+++ src/main/java/br/edu/ifpb/tcc1/web/controller/IntervaloController.java
@@ -2,6 +2,8 @@ package br.edu.ifpb.tcc1.web.controller;
 
 import br.edu.ifpb.tcc1.web.dao.QueryEmpenhosJDBC;
 import br.edu.ifpb.tcc1.web.graficos.GraficoPizza;
+import java.time.Month;
+import java.time.YearMonth;
 
 public class IntervaloController {
 
@@ -13,7 +15,19 @@ public class IntervaloController {
 
     public GraficoPizza buscaPorMes(int mes1, int mes2) {
 
-        GraficoPizza grafico = new GraficoPizza("busca por meses");
+        String primeiromes = String.valueOf(mes1);
+        String segundoomes = String.valueOf(mes2);
+        int primeiroano = Integer.parseInt(primeiromes.substring(0, 4));
+        int segundoano = Integer.parseInt(segundoomes.substring(0, 4));
+
+        YearMonth ym1 = YearMonth.of(primeiroano, 
+                Integer.parseInt(primeiromes.substring(4, 6)));
+        YearMonth ym2 = YearMonth.of(primeiroano, 
+                Integer.parseInt(primeiromes.substring(4, 6)));
+        
+        
+        GraficoPizza grafico = new GraficoPizza("Busca por gastos entre meses");
+
         grafico.setData(query.buscaPorMes(mes1, mes2));
         grafico.setName("mes");
 
--- src/main/webapp/js/intervalos.js
+++ src/main/webapp/js/intervalos.js
@@ -5,8 +5,8 @@ btSemestre.onclick = function () {
     let semestres;
     fecharmodal(document.getElementById("modalSemestre"));
     let url = 'http://localhost:8080/tcc1-web/api/intervalo' +
-        '/semestre/' + document.getElementById('semestre').value
-        + anoSemestre.value;
+            '/semestre/' + document.getElementById('semestre').value
+            + anoSemestre.value;
 
     console.log(url);
     client.open('GET', url, false);
@@ -14,7 +14,8 @@ btSemestre.onclick = function () {
     console.log(client.status);
     if (client.status == 200) {
         semestre = JSON.parse(client.responseText);
-
+    } else {
+        alert('valores indisponíveis');
     }
 
     intervalos(semestre);
@@ -22,19 +23,41 @@ btSemestre.onclick = function () {
 
 btAnos.onclick = function () {
     fecharmodal(document.getElementById("modalAno"));
-    
+
     let json;
     let client = new XMLHttpRequest();
     let url = 'http://localhost:8080/tcc1-web/api/intervalo' +
-        '/anos/' + ano1.value + "/" + ano2.value;
-
+            '/anos/' + ano1.value + "/" + ano2.value;
+    client.send()
     client.open('GET', url, false);
     client.send(null);
     console.log(client.status);
     if (client.status == 200) {
         json = JSON.parse(client.responseText);
         console.log(JSON.stringify(json));
+    } else {
+        alert('valores indisponíveis');
     }
 
     intervalos(json);
+}
+
+btMeses.onclick = function () {
+    fecharmodal(document.getElementById("modalMes"));
+
+    let json;
+    let client = new XMLHttpRequest();
+    let url = 'http://localhost:8080/tcc1-web/api/intervalo' +
+            '/mes/' + anoMes1.value + mes1.value + "/" + anoMes2.value + mes2.value;
+    client.open('GET', url, false);
+    client.send(null);
+    if (client.status == 200) {
+        json = JSON.parse(client.responseText);
+        console.log(JSON.stringify(json));
+        intervalos(json);
+    } else {
+        alert('valores indisponíveis');
+    }
+
+
 }
