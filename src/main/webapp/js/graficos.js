d9ffe2a689e8065da2ab2ef6a08671b13ccbbd11
FlavioHenriique <flaviohenrique638@gmail.com>
2018-09-16 12:26:21 -0300

    Adicionando evento de clique para cada item do gráfico de pizza

--- src/main/webapp/js/graficos.js
+++ src/main/webapp/js/graficos.js
@@ -1,34 +1,38 @@
-function intervalos(json){
+function intervalos(json, clique) {
+    console.log(JSON.stringify(clique));
+    
     Highcharts.chart('container', {
-                    chart: {
-                        type: 'pie',
-                        options3d: {
-                            enabled: true,
-                            alpha: 45,
-                            beta: 0
-                        }
-                    },
-                    title: {
-                        text: json.title
-                    },
-                    tooltip: {
-                        pointFormat: '{series.name}: <b>{point.y} R$</b>'
-                    },
-                    plotOptions: {
-                        pie: {
-                            allowPointSelect: true,
-                            cursor: 'pointer',
-                            depth: 35,
-                            dataLabels: {
-                                enabled: true,
-                                format: '{point.name}'
-                            }
-                        }
-                    },
-                    series: [{
-                            type: 'pie',
-                            name: json.name,
-                            data: json.data
-                        }]
-                });
+        chart: {
+            type: 'pie',
+            options3d: {
+                enabled: true,
+                alpha: 45,
+                beta: 0
+            }
+        },
+        title: {
+            text: json.title
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.y} R$</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                colorByPoint: true,
+                cursor: 'pointer',
+                depth: 35,
+                showInLegend: true,
+                dataLabels: {
+                    enabled: true,
+                    format: '{point.name}'
+                },
+            }},
+        series: [{
+                type: 'pie',
+                name: json.name,
+                data: json.data,
+                point:clique
+            }]
+    });
 }
--- src/main/webapp/js/intervalos.js
+++ src/main/webapp/js/intervalos.js
@@ -1,12 +1,41 @@
 let client = new XMLHttpRequest();
 
+btAnos.onclick = function () {
+    fecharmodal(document.getElementById("modalAno"));
+
+    let json;
+    let client = new XMLHttpRequest();
+    let url = 'http://localhost:8080/tcc1-web/api/intervalo' +
+        '/anos/' + ano1.value + "/" + ano2.value;
+
+    client.open('GET', url, false);
+    client.send(null);
+    console.log(client.status);
+    if (client.status == 200) {
+        json = JSON.parse(client.responseText);
+        console.log(JSON.stringify(json));
+
+        let clique = {
+            events: {
+                click: function (event) {
+                    alert("ano " + this.name);
+                }
+            }
+        };
+        intervalos(json, clique);
+
+    } else {
+        alert('valores indisponíveis');
+    }
+}
+
 
 btSemestre.onclick = function () {
     let semestres;
     fecharmodal(document.getElementById("modalSemestre"));
     let url = 'http://localhost:8080/tcc1-web/api/intervalo' +
-            '/semestre/' + document.getElementById('semestre').value
-            + anoSemestre.value;
+        '/semestre/' + document.getElementById('semestre').value
+        + anoSemestre.value;
 
     console.log(url);
     client.open('GET', url, false);
@@ -14,33 +43,22 @@ btSemestre.onclick = function () {
     console.log(client.status);
     if (client.status == 200) {
         semestre = JSON.parse(client.responseText);
+
+        let clique = {
+            events: {
+                click: function (event) {
+                    alert("semestre " + this.name);
+                }
+            }
+        };
+        intervalos(semestre, clique);
+
     } else {
         alert('valores indisponíveis');
     }
 
-    intervalos(semestre);
 }
 
-btAnos.onclick = function () {
-    fecharmodal(document.getElementById("modalAno"));
-
-    let json;
-    let client = new XMLHttpRequest();
-    let url = 'http://localhost:8080/tcc1-web/api/intervalo' +
-            '/anos/' + ano1.value + "/" + ano2.value;
-    client.send()
-    client.open('GET', url, false);
-    client.send(null);
-    console.log(client.status);
-    if (client.status == 200) {
-        json = JSON.parse(client.responseText);
-        console.log(JSON.stringify(json));
-    } else {
-        alert('valores indisponíveis');
-    }
-
-    intervalos(json);
-}
 
 btMeses.onclick = function () {
     fecharmodal(document.getElementById("modalMes"));
@@ -48,16 +66,25 @@ btMeses.onclick = function () {
     let json;
     let client = new XMLHttpRequest();
     let url = 'http://localhost:8080/tcc1-web/api/intervalo' +
-            '/mes/' + anoMes1.value + mes1.value + "/" + anoMes2.value + mes2.value;
+        '/mes/' + anoMes1.value + mes1.value + "/" + anoMes2.value + mes2.value;
     client.open('GET', url, false);
     client.send(null);
     if (client.status == 200) {
         json = JSON.parse(client.responseText);
         console.log(JSON.stringify(json));
-        intervalos(json);
+
+        let clique = {
+            events:{
+                click: function (event) {
+                    alert("meses "+ this.name);
+                }
+            }
+        };
+        intervalos(json,clique);
+
     } else {
         alert('valores indisponíveis');
     }
 
-
+    
 }
